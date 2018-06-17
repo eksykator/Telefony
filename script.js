68f71758510f8d16f5ef2ad1d3ce2f75660fd881
Marta Bobowska <martabobowska2@interia.pl>
2018-06-17 15:59:27 +0200

    Add resolution property

--- script.js
+++ script.js
@@ -2,17 +2,28 @@ function Phone(brand, price, color) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
+    this.resolution = {
+        width: 1080,
+        height: 1920
+    };
 }
 
 Phone.prototype.printInfo = function() {
 	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 }
 
+Phone.prototype.printResolution = function() {
+    console.log("The resolution phone is: " + this.resolution.width + ' x ' + this.resolution.height + '.');
+}
+
 var iPhone6S = new Phone("Apple", 2250, "silver");
 iPhone6S.printInfo();
+iPhone6S.printResolution();
 
 var samsungS6 = new Phone("Samsung Galaxy S6", 1000, "silver");
 samsungS6.printInfo();
+samsungS6.printResolution();
 
 var onePlusOne = new Phone("OnePlus One", 1299, "black");
 onePlusOne.printInfo();
+onePlusOne.printResolution();
